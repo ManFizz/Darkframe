9da883f986a5438dc9d12c2d94b816bdbdf00aaa
ManFis <vovka79025315990@gmail.com>
2024-01-24 17:44:29 +1000

    p365 logic is placed in a separate file

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
-import { ClearGallery, BuildThumbBySrc, DisplayImagesByPath } from './thumb.mjs';
+import { ClearGallery, DisplayImagesByPath } from './thumb.mjs';
+import {DisplayP365} from "./p365.js";
 
 $('#sidebarCollapse').on('click', function() {
     $('.sidebar').toggleClass('open');
@@ -38,65 +39,6 @@ export function SetNavActive(selector)
     });
     document.querySelector(selector).parentNode.classList.add("active");
 }
-
-
-
-function P365GetVideoByURL(url)
-{
-    const x = new XMLHttpRequest();
-    x.overrideMimeType('text/html');
-    x.open("GET", url, true);
-    x.onreadystatechange = function () {
-        if (x.readyState !== 4 || x.status !== 200)
-            return;
-
-        let parser = new DOMParser();
-        let xmlDoc = parser.parseFromString(x.responseText, "text/html");
-        BuildThumbBySrc(xmlDoc.querySelector("head > meta[property='og:image']").content, xmlDoc.querySelector("head > meta[property='og:video']").content);
-    };
-    x.send(null);
-}
-
-
-let P365currentDomain = 'wow';
-let P365protocol = 'http://'
-
-function DisplayP365() {
-    const url = P365protocol + P365currentDomain + '.porno365.bond/user/176139';
-    const x = new XMLHttpRequest();
-    x.overrideMimeType('application/xml');
-    x.open("GET", url, true);
-    x.onreadystatechange = function () {
-        if (x.readyState !== 4 || x.status !== 200)
-            return;
-
-
-        let parser = new DOMParser();
-        let xmlDoc = parser.parseFromString(x.responseText, "text/html")
-        let urls = xmlDoc.querySelectorAll(".video_block a");
-        if(urls != null) {
-
-            SetNavActive("#nav-p365");
-
-            ClearGallery();
-
-            urls.forEach(link => {
-                let l = link.getAttribute('href');
-                P365GetVideoByURL(l);
-            });
-        }
-    };
-    x.send(null);
-}
-
-/*
-function P365GetTrailerURL(id)
-{
-    let trailerUrl = P365protocol + '//' + trailersServerN[n.id % 10] + '.' + P365currentDomain + '/porno365/trailers/' + id[0] + '/' +id[1] + '/' + id + '.webm';
-    if (n.id < 10) {
-        trailerUrl = P365protocol + '//' + trailersServerN[n.id % 10] + '.' + P365currentDomain + '/porno365/trailers/0/' + id + '.webm';
-    }
-}*/
 
 function ChangeSection(section) {
     let sidebar = document.querySelector('.sidebar');
---
+++ src/js/p365.js
@@ -0,0 +1,60 @@
+import {BuildThumbBySrc, ClearGallery} from "./thumb.mjs";
+import {SetNavActive} from "./main.js";
+
+
+function P365GetVideoByURL(url)
+{
+    const x = new XMLHttpRequest();
+    x.overrideMimeType('text/html');
+    x.open("GET", url, true);
+    x.onreadystatechange = function () {
+        if (x.readyState !== 4 || x.status !== 200)
+            return;
+
+        let parser = new DOMParser();
+        let xmlDoc = parser.parseFromString(x.responseText, "text/html");
+        BuildThumbBySrc(xmlDoc.querySelector("head > meta[property='og:image']").content, xmlDoc.querySelector("head > meta[property='og:video']").content);
+    };
+    x.send(null);
+}
+
+
+let P365currentDomain = 'wow';
+let P365protocol = 'http://'
+
+export function DisplayP365() {
+    const url = P365protocol + P365currentDomain + '.porno365.bond/user/176139';
+    const x = new XMLHttpRequest();
+    x.overrideMimeType('application/xml');
+    x.open("GET", url, true);
+    x.onreadystatechange = function () {
+        if (x.readyState !== 4 || x.status !== 200)
+            return;
+
+
+        let parser = new DOMParser();
+        let xmlDoc = parser.parseFromString(x.responseText, "text/html")
+        let urls = xmlDoc.querySelectorAll(".video_block a");
+        if(urls != null) {
+
+            SetNavActive("#nav-p365");
+
+            ClearGallery();
+
+            urls.forEach(link => {
+                let l = link.getAttribute('href');
+                P365GetVideoByURL(l);
+            });
+        }
+    };
+    x.send(null);
+}
+
+/*
+function P365GetTrailerURL(id)
+{
+    let trailerUrl = P365protocol + '//' + trailersServerN[n.id % 10] + '.' + P365currentDomain + '/porno365/trailers/' + id[0] + '/' +id[1] + '/' + id + '.webm';
+    if (n.id < 10) {
+        trailerUrl = P365protocol + '//' + trailersServerN[n.id % 10] + '.' + P365currentDomain + '/porno365/trailers/0/' + id + '.webm';
+    }
+}*/
