4ae0c2e79c77921c4b6bc047ab9d4c4c3a78bdc7
ManFis <vovka79025315990@gmail.com>
2024-02-11 09:51:44 +0800

    add sort functional

--- src/js/thumb.js
+++ src/js/thumb.js
@@ -7,6 +7,38 @@ let gallery = document.querySelector("#gallery");
 
 let current_remote_type = 1;
 
+document.addEventListener('DOMContentLoaded', function() {
+    let btnOrderSort = document.getElementById("btn-order-sort");
+    btnOrderSort.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        if (typeOrder !== 'asc') {
+            typeOrder = 'asc';
+        } else typeOrder = 'desc';
+
+        let icon = btnOrderSort.querySelector('.fa');
+        if (icon) {
+            icon.className = 'fa fa-sort-' + typeOrder;
+            icon.outerHTML += "";
+        }
+
+        SortFolderDisplay();
+    });
+
+    let btnTypeSort = document.getElementById('btn-type-sort');
+    btnTypeSort.innerText = typeSort;
+    btnTypeSort.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        if(typeSort === 'name'){
+            typeSort = 'time'
+        } else typeSort = 'name';
+
+        btnTypeSort.innerText = typeSort;
+
+        SortFolderDisplay();
+    });
+});
 export function ClearGallery()
 {
     while (gallery.childNodes.length > 0)
@@ -70,6 +102,19 @@ export function BuildThumbBySrc(thumbUrl, remote_type, openModalUrl = null, tags
     let blockElem = newDisplayFile.GetThumb();
     blockElem.setAttribute("id-list", imageList.indexOf(newDisplayFile).toString());
 
+    if(newDisplayFile instanceof ImageFile) {
+        let img = blockElem.querySelector("img");
+        img.onload = async  () => {
+            // Проверка, что изображение успешно загружено
+            if (img.complete && img.naturalWidth > 0) {
+                let anim = await IsAnimated(img.src);
+                if(!anim)
+                    resizeImage(img);
+                img.onload = null;
+            }
+        };
+    }
+
     let overlay = document.createElement("div");
     overlay.classList.add("overlay");
     buildOverlay(overlay, newDisplayFile);
@@ -124,6 +169,17 @@ export function BuildThumbFolder(path, name) {
     gallery.insertBefore(blockElem, gallery.firstElementChild);
 }
 
+let typeSort = 'time'; // 'name' или 'time'
+let typeOrder = 'asc'; // 'asc' или 'desc'
+
+const sortFunction = (a, b) => {
+    if (typeSort === 'name') {
+        return typeOrder === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
+    } else if (typeSort === 'time') {
+        return typeOrder === 'asc' ? a.time - b.time : b.time - a.time;
+    }
+};
+
 export async function DisplayImagesByPath(path)
 {
     let mainContainer = document.querySelector("#main-container");
@@ -140,16 +196,103 @@ export async function DisplayImagesByPath(path)
 
     ClearGallery();
     let arr = JSON.parse(responseText);
-    arr.forEach(src => {
-        if(src.includes('.nomedia'))
+    const sortedArr = arr.sort(sortFunction);
+
+    sortedArr.forEach(item => {
+        const { name, time } = item;
+        if(name.includes('.nomedia'))
             return;
 
-        if(!src.includes('.'))
+        if(!name.includes('.')) //is folder
         {
-            BuildThumbFolder(path, src);
+            BuildThumbFolder(path, name);
             return;
         }
-        BuildThumbBySrc(path + "\\" + src, 1);
+
+        let absPath = path + "\\" + name;
+        BuildThumbBySrc(absPath, 1, null, null, null, name, time);
     });
     BuildThumbReturn(path);
 }
+
+function getValue(object) {
+    if(object === undefined)
+        throw "wtf" + object + " | " + typeof object;
+
+    if(typeSort === 'time')
+        return object.time;
+
+    if(typeSort === 'name')
+        return object.title || object.thumbUrl;
+
+    throw "undef typeSort: [" + typeOrder + "]";
+}
+
+function SortFolderDisplay() {
+    const gallery = document.getElementById('gallery');
+    const thumbs = gallery.querySelectorAll('.thumb');
+
+    const sortedThumbs = Array.from(thumbs).sort(sortFunc);
+
+    sortedThumbs.forEach(thumb => {
+        gallery.insertBefore(thumb, gallery.firstChild);
+    });
+}
+
+
+function sortFunc(a, b) {
+    const idListA = parseInt(a.getAttribute("id-list"));
+    const idListB = parseInt(b.getAttribute("id-list"));
+
+    const validA = IsValidDisplayId(idListA);
+    const validB = IsValidDisplayId(idListB);
+
+    if (validA && validB) {
+        const valueA = getValue(imageList[idListA]),
+            valueB  = getValue(imageList[idListB]);
+
+        if (typeOrder === 'name') {
+            return valueA.localeCompare(valueB);
+        } else {
+            return valueA - valueB;
+        }
+    } else if (validA && !validB) {
+        return -1;
+    } else if (!validA && validB) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function IsValidDisplayId(id) {
+    if(isNaN(id))
+        return false;
+
+    let object = imageList[id];
+    return object !== null && object !== undefined;
+}
+
+
+function resizeImage(originalImage) {
+    const maxDimension = 1080;
+
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    const naturalWidth = originalImage.naturalWidth;
+    const naturalHeight = originalImage.naturalHeight;
+
+    const maxDim = Math.max(naturalWidth, naturalHeight);
+
+    const newWidth = naturalWidth * (maxDimension / maxDim);
+    const newHeight = naturalHeight * (maxDimension / maxDim);
+
+    canvas.width = newWidth;
+    canvas.height = newHeight;
+
+    context.drawImage(originalImage, 0, 0, canvas.width, canvas.height);
+
+    originalImage.src = canvas.toDataURL('image/jpeg'); // Используйте 'image/png', если нужен формат PNG;
+}
+
