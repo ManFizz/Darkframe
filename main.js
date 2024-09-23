e8dc52b4771abc6d7742413f67968aa3b63d6e37
ManFizz <vovka79025315990@gmail.com>
2024-09-23 13:20:50 +0800

    try to fix network errors

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const {app, BrowserWindow} = require('electron');
 const { backupDatabase } = require('./server/backup');
 const { setupIpcHandlers } = require('./server/ipcManager');
+const { downloadMissingFavorites } = require('./server/controllers/favoriteController');
+const { session } = require('electron')
+
+app.commandLine.appendSwitch('disable-quic');
+app.commandLine.appendSwitch('disable-http2');
+app.commandLine.appendSwitch('ignore-certificate-errors');
+app.commandLine.appendSwitch('ignore-ssl-errors');
 
 let openDevTools = false;
 for (let i = 0; i < process.argv.length; i++) {
@@ -18,7 +25,9 @@ function createWindow() {
         icon: __dirname + "/img/icon.svg",
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false
+            contextIsolation: false,
+            allowDisplayingInsecureContent: true,
+            allowRunningInsecureContent: true,
         }
     });
     win.maximize();
@@ -33,6 +42,23 @@ backupDatabase();
 app.whenReady().then(() => {
     setupIpcHandlers();
     createWindow();
+
+    const filter = {
+        urls: ['*']
+    }
+
+    session.defaultSession.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
+        details.requestHeaders['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'
+        callback({ requestHeaders: details.requestHeaders })
+    })
+
+    app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
+        event.preventDefault()
+        callback(true)
+
+    })
+
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 });
 
 app.on('window-all-closed', () => {
